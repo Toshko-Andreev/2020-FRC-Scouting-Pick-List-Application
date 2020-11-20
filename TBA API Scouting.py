3e60f99b411347c9611b17c32c71366b171413c4
Toshko-Andreev <52345724+Toshko-Andreev@users.noreply.github.com>
2020-11-20 00:25:26 -0800

    Add files via upload

--- TBA API Scouting.py
+++ TBA API Scouting.py
@@ -1,19 +1,11 @@
 import requests
+from operator import index
 #team/frc2658/event/2020cadm/matches/keys
 r = requests.get('https://www.thebluealliance.com/api/v3/match/2020cadm_qm87', params = {'X-TBA-Auth-Key': 'ZxhuACw5seFvz5VqNxNGKG9yC9sbDZshpjM4PisgEgVc533hlJQSp20zDrYZe1FX'})
-
+first = requests.get('https://www.thebluealliance.com/api/v3/team/frc2658/event/2020cadm/matches/keys', params = {'X-TBA-Auth-Key': 'ZxhuACw5seFvz5VqNxNGKG9yC9sbDZshpjM4PisgEgVc533hlJQSp20zDrYZe1FX'})
 the_string = r.text
-
-array = []
-for i in the_string:
-    array.append(i)
-
-print(the_string)
-#print(array)
-
+#print(the_string)
 #----------------------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------------------
-#Next part is doing the outer and inner cell calculations for the blue alliance
 #----------------------------------------------------------------------------------------------------------------
 
 def find(list, word):
@@ -68,27 +60,6 @@ def get_value(list, word):
 #----------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------------
 
-firstblue = find(array, "blue")
-secondblue = find_range(array, "blue", firstblue, "end")
-
-firstred = find(array, "red")
-secondred = find_range(array, ['r','e','d','"'], firstred, "end")
-
-endred = 0
-for i in range(secondred,len(array),1):
-    if array[i]=='}':
-        endred = i
-        break
-
-bluelist2 = []
-redlist2 = []
-
-for i in range(secondblue,secondred):
-    bluelist2.append(array[i])
-
-for i in range(secondred,endred):
-    redlist2.append(array[i])
-
 def match_red(in_string):
     array = []
     for i in in_string:
@@ -118,8 +89,52 @@ def match_blue(in_string):
         bluelist.append(array[i])
     return bluelist
 
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+
+def store_keys(in_string):
+    array = []
+    for i in in_string:
+        array.append(i)
+    how_many = 0
+    for ch in array:
+        if ch == '"':
+            how_many += 1
+    add = False
+    key_list = []
+    startpoint = 0
+    for num in range(0,how_many):
+        key = ""
+        for chn in range(startpoint,len(array)):
+            if add == False and array[chn] == '"':
+                add = True
+                startpoint = chn+1
+                break
+            elif add == True and array[chn] == '"':
+                add = False
+                key_list.append(key)
+                startpoint = chn+1
+                break
+            elif add == False:
+                nothing = 0
+            else:
+                key += array[chn]
+    return key_list
+
+
+
 print("--------------------------------------------------------")  
-print(get_value(redlist2, "teleopPoints"))
-print(get_value(match_red(the_string), "teleopPoints"))
-print(get_value(bluelist2, "teleopPoints"))
-print(get_value(match_blue(the_string), "teleopPoints"))
+#print(store_keys(first.text))
+#print(get_value(match_red(the_string), "teleopPoints"))
+#print(get_value(match_blue(the_string), "teleopPoints"))
+
+for i in store_keys(first.text):
+    match = requests.get('https://www.thebluealliance.com/api/v3/match/' + i, params = {'X-TBA-Auth-Key': 'ZxhuACw5seFvz5VqNxNGKG9yC9sbDZshpjM4PisgEgVc533hlJQSp20zDrYZe1FX'})
+    rtp = get_value(match_red(match.text), "teleopCellsOuter")
+    rip = get_value(match_red(match.text), "teleopCellsInner")
+    btp = get_value(match_blue(match.text), "teleopCellsOuter")
+    bip = get_value(match_red(match.text), "teleopCellsInner")
+    print("Match: " + i)
+    print("Red Top: " + str(rtp+rip))
+    print("Blue Top: " + str(btp+bip))
+    print("--------------------------------------------------------")
